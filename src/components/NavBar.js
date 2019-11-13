0463bf4907e9303451e53472717e300772c4b5e3
Deepa Kiwne <deepa.kiwne@gmail.com>
2019-11-13 07:02:30 -0700

    feat: Display avatar and logout button after login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,16 +55,20 @@ class NavBar extends Component {
                         </Link>
                     </li>
                 </ul>
+                {name === null
+                ? null
+                : 
                 <form className="form-inline my-2 my-lg-0">
                     <li className="nav-item">
-                        <p className="nav-link disabled">{name === null ? '' : `Hello ${name}!`}</p>
+                        <p className="nav-link disabled">{`Hello ${name}!`}</p>
                     </li>
                     <li className="nav-item">
                         <img src={avatar === null ? avatarPlaceHolder : avatar}
                             alt="Avatar" className="avatar"></img>
                     </li>
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.onLogout}>Logout</button>
-                </form>
+                </form>}
+
             </div>
         </nav>
     )
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,6 +5,14 @@ class NotFound extends Component {
   render() {
 
     return (
+
+      <div>
+      <div class="card text-center borderNone">
+        <div class="card-body">
+          <span className='card-text' style={{fontSize: 20}}>Please SignIn!</span>
+        </div>
+      </div>
+
       <div className="card" style={{width: '40rem'}}>
         <div className="card-header h6 text-center display-1">
           <strong>404</strong>
@@ -12,6 +20,7 @@ class NotFound extends Component {
         <div className="card-body">
           <p className='text-center display-4'>NOT FOUND</p>
         </div>
+      </div>
       </div>
     )
   }
